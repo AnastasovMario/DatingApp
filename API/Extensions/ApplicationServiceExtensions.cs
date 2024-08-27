5710b5560e64b5d4e162a849887f8138f348eb5a
AnastasovMario <marioanastasov5@gmail.com>
2024-08-27 13:09:52 +0300

    Adding Extensions

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+//Static allows us to use the methods inside a class without needing to create a new instance
+public static class ApplicationServiceExtensions
+{
+    // The goal is to write services.AddApplicationServices, so we use (this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddControllers();
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddCors();
+        services.AddScoped<ITokenService, TokenService>(); //This gives an abstraction and makes the service more testable
+
+        return services;
+    }
+}
