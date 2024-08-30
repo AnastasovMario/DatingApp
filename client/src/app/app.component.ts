87e60cee7e735afde7a69974414b044eee0f6248
AnastasovMario <marioanastasov5@gmail.com>
2024-08-30 11:48:42 +0300

    Connecting child parent to child components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component"; //in this way we are adding the nav component to the app
@@ -14,13 +13,9 @@ import { HomeComponent } from "./home/home.component";
 })
 
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
   private accountService = inject(AccountService);
-  title = 'DatingApp';
-  users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -31,12 +26,5 @@ export class AppComponent implements OnInit {
     this.accountService.currentUser.set(user);
   }
 
-  getUsers(){
-    //http get always completes, so we don't have to unsubscribe
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
+
 }
