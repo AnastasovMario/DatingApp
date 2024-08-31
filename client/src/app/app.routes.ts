e349394333c3119407ad366a4cb35669e579aac1
AnastasovMario <marioanastasov5@gmail.com>
2024-08-31 09:34:13 +0300

    Routing guard added

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './__guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'members', component: MemberListComponent},
+    {path: 'members', component: MemberListComponent, canActivate: [authGuard]},
     {path: 'member/:id', component: MemberListComponent},
     {path: 'lists', component: ListsComponent},
     {path: 'messages', component: MessagesComponent},
     {path: '**', component: HomeComponent, pathMatch: 'full'},
+
 
 ];
