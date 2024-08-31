65ccd16e43877a2604af7a5efec3d7c262e085f0
AnastasovMario <marioanastasov5@gmail.com>
2024-08-31 09:26:05 +0300

    Add routing navigation and toastr service

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -30,7 +30,6 @@ export class AccountService {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUser.set(user);
         }
-
         return user;
       })
     )
