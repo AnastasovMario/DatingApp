da5a584df49b3caa67f1137ace35c5d2df3fa60e
AnastasovMario <marioanastasov5@gmail.com>
2024-08-31 12:56:28 +0300

    Add test-errors component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './__guards/auth.guard';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -18,5 +19,6 @@ export const routes: Routes = [
             {path: 'messages', component: MessagesComponent}
         ]
     },
+    {path: 'errors', component: TestErrorsComponent},
     {path: '**', component: HomeComponent, pathMatch: 'full'},
 ];
